e5c42b9b17705d91a472e9289b201dd8b0b6a3cd
AbobakarSadeeq <abobakarkhan7@gmail.com>
2025-10-24 00:18:46 +0500

    bootstrap added and model also for add relaxing sound section.

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,27 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
+  imports: []
 })
+
 export class Home {
 
   playSoundBtn = false;
-  backgroundImage: string = 'rain-img.jpg' // ../../public/rain-img.jpg and rain-img.jpg are same
+  backgroundImage: string = 'rain-img.jpg';
+
+  ngOnInit(): void {
+
+  }
 
   playSound() {
-    console.log("asdfg");
+    console.log("play sound");
     this.playSoundBtn = true;
   }
 
   stopSound(): void {
-    console.log("zxcv");
+    console.log("stop sound");
     this.playSoundBtn = false;
   }
 
-  addRelaxingSound(): void {
-    console.log("not yet!");
+  removeSound(): void {
+    console.log('remove sound');
+  }
+
+  // ********************** Add relaxing model sound section **********************
+
+  @ViewChild('AddRelaxingSoundModal') AddRelaxingSoundModalRef!: ElementRef;
+  private _bootstrapModalInstance: any;
+
+  ngAfterViewInit(): void {
+    this._bootstrapModalInstance = new bootstrap.Modal(this.AddRelaxingSoundModalRef.nativeElement);
+  }
+
+  openAddRelaxingSoundModel(): void {
+    console.log("on open relaxing sound model done!");
+    this._bootstrapModalInstance.show();
+
+  }
+
+  onCloseRelaxingSoundModel(): void {
+    console.log("on close relaxing sound model done!");
+    this._bootstrapModalInstance.hide();
+  }
+
+  onAddRelaxingSound(): void {
+    console.log("on add relaxing sound done!");
+    this._bootstrapModalInstance.hide();
+
   }
 }
