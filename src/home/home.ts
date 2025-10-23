489523be6898cb4662fcb734e93ba42723da9c86
AbobakarSadeeq <abobakarkhan7@gmail.com>
2025-10-23 20:11:13 +0500

    some ui updated of releaxing sound section.

--- src/home/home.ts
+++ src/home/home.ts
@@ -9,16 +9,19 @@ import { Component } from '@angular/core';
 export class Home {
 
   playSoundBtn = false;
+  backgroundImage: string = 'rain-img.jpg' // ../../public/rain-img.jpg and rain-img.jpg are same
 
   playSound() {
     console.log("asdfg");
     this.playSoundBtn = true;
   }
 
-  stopSound():void {
+  stopSound(): void {
     console.log("zxcv");
     this.playSoundBtn = false;
+  }
 
-
+  addRelaxingSound(): void {
+    console.log("not yet!");
   }
 }
