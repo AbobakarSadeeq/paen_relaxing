e29bf3a60672a5aced09a0a74fdb84d63df0f8ea
AbobakarSadeeq <abobakarkhan7@gmail.com>
2025-10-26 15:08:40 +0500

    selection relaxing sound  model almost done.

---
+++ src/home/home.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllCategoriesWithRelaxingSounds(): Observable<any> {
+    return this.http.get("/json/relaxing-sound-data.json");
+  }
+}
--- src/home/home.ts
+++ src/home/home.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HomeService } from './home.service';
+// import { HomeService } from './home.service';
 
 declare var bootstrap: any;
 
@@ -13,9 +15,16 @@ export class Home {
 
   playSoundBtn = false;
   backgroundImage: string[] = ['/Relaxing-Sounds-images/Rain/rain-img.jpg', '/Relaxing-Sounds-images/Rain/thunderstorm-rain.jpg'];
+  categoriesWithItsRelaxingSounds: any[] = [];
+
+
+  constructor(private _homeService: HomeService) { }
 
   ngOnInit(): void {
-
+    this._homeService.getAllCategoriesWithRelaxingSounds().subscribe((data: any) => {
+      console.log(data);
+      this.categoriesWithItsRelaxingSounds = data;
+    });
   }
 
   playSound() {
@@ -36,7 +45,8 @@ export class Home {
 
   @ViewChild('AddRelaxingSoundModal') AddRelaxingSoundModalRef!: ElementRef;
   private _bootstrapModalInstance: any;
-  isAddRelaxingSoundSelected = false;
+  addOnSelectedRelaxingSoundId = 0;
+  selectedRelaxingSoundAudioObjInSelections: HTMLAudioElement | null = null;
 
   ngAfterViewInit(): void {
     this._bootstrapModalInstance = new bootstrap.Modal(this.AddRelaxingSoundModalRef.nativeElement);
@@ -51,16 +61,31 @@ export class Home {
   onCloseRelaxingSoundModel(): void {
     console.log("on close relaxing sound model done!");
     this._bootstrapModalInstance.hide();
+    this.addOnSelectedRelaxingSoundId = 0;
+    this.selectedRelaxingSoundAudioObjInSelections?.pause();
   }
 
   onAddRelaxingSound(): void {
     console.log("on add relaxing sound done!");
     this._bootstrapModalInstance.hide();
+    this.addOnSelectedRelaxingSoundId = 0;
+    this.selectedRelaxingSoundAudioObjInSelections?.pause();
   }
 
-  abc(): void {
-    console.log("forst");
-    this.isAddRelaxingSoundSelected = !this.isAddRelaxingSoundSelected;
+  selectingRelaxingSoundOnSelectionModel(selectedRelaxingSoundId: number, selectedCategoryIndex: number, selectedRelaxingSoundIndex: number): void {
+    if (selectedRelaxingSoundId === this.addOnSelectedRelaxingSoundId) {
+      // stop if same selected sound pressed again and also change the selection style as well.
+      this.addOnSelectedRelaxingSoundId = 0;
+      this.selectedRelaxingSoundAudioObjInSelections?.pause();
+    }
+    else {
+      // change select style effect and change the relaxing sound with it as well.
+      this.addOnSelectedRelaxingSoundId = selectedRelaxingSoundId;
+      this.selectedRelaxingSoundAudioObjInSelections?.pause();
+      this.selectedRelaxingSoundAudioObjInSelections = new Audio(this.categoriesWithItsRelaxingSounds[selectedCategoryIndex].relaxingSounds[selectedRelaxingSoundIndex].soundAudioLocation);
+      this.selectedRelaxingSoundAudioObjInSelections.play();
+    }
+
   }
 
 }
